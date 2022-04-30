890ce7888cc20c46f9d5a676eee916b844dfee04
Patrick Steele <steele.pat@gmail.com>
2022-04-30 17:43:34 -0400

    Support converting to a line-graph like representation

--- conlog/datatypes.py
+++ conlog/datatypes.py
@@ -5,40 +5,40 @@ from typing import Iterable
 import networkx as nx
 
 
-@dataclass(frozen=True)
+@dataclass(frozen=True, order=True)
 class Operation:
     pass
 
 
-@dataclass(frozen=True)
+@dataclass(frozen=True, order=True)
 class Initial(Operation):
     free: tuple[str, ...]
     fixed: tuple[tuple[str, int], ...]
 
 
-@dataclass(frozen=True)
+@dataclass(frozen=True, order=True)
 class Terminal(Operation):
     pass
 
 
-@dataclass(frozen=True)
+@dataclass(frozen=True, order=True)
 class Addition(Operation):
     lhs: str
     rhs: str | int
 
 
-@dataclass(frozen=True)
+@dataclass(frozen=True, order=True)
 class Print(Operation):
     var: str
 
 
-@dataclass(frozen=True)
+@dataclass(frozen=True, order=True)
 class Subtraction(Operation):
     lhs: str
     rhs: str | int
 
 
-@dataclass(frozen=True)
+@dataclass(frozen=True, order=True)
 class Node:
     name: str
     op: Operation | None
---
+++ conlog/directed.py
@@ -0,0 +1,20 @@
+import networkx as nx
+
+
+def make_directed(g: nx.Graph) -> nx.DiGraph:
+    """Create a directed copy of a graph where u-turns are not legal."""
+
+    nodes = list(g.edges)
+    nodes.extend((v, u) for u, v in g.edges)
+
+    edges = []
+    for u, v in nodes:
+        for w in g.neighbors(v):
+            if w != u:
+                edges.append(((u, v), (v, w)))
+
+    directed = nx.DiGraph()
+    directed.add_nodes_from(nodes)
+    directed.add_edges_from(edges)
+
+    return directed
