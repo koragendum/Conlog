31543be407dbf989afbc527698ddb82ab4f9de13
Patrick Steele <steele.pat@gmail.com>
2022-04-30 15:11:33 -0400

    Support Nix

---
+++ shell.nix
@@ -0,0 +1,19 @@
+{ sources ? import ./nix/sources.nix
+, pkgs ? import sources.nixpkgs { }
+}:
+let
+  my-python-packages = python-packages: with python-packages; [
+    isort
+    flake8
+    mccabe
+    rope
+    pyflakes
+    pytest
+    hypothesis
+    line_profiler
+  ];
+  python-with-my-packages = pkgs.python310.withPackages my-python-packages;
+in
+pkgs.mkShell {
+  buildInputs = [ python-with-my-packages pkgs.nodePackages.pyright ];
+}
