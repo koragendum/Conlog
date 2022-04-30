b4268dbe647ab875ee99431e2ea742fa2236a345
Patrick Steele <steele.pat@gmail.com>
2022-04-30 15:11:11 -0400

    Correct test types

--- tests/triangle_sum.py
+++ tests/triangle_sum.py
@@ -1,5 +1,13 @@
-from conlog.datatypes import Operation, Initial, Terminal, Addition, Print, Subtraction, Node, Graph
-
+from conlog.datatypes import (
+    Addition,
+    Graph,
+    Initial,
+    Node,
+    Operation,
+    Print,
+    Subtraction,
+    Terminal,
+)
 
 """
 Graph that computes either T=triangle_sum(x) or T=triangle_sum(x-1)
@@ -11,26 +19,23 @@ depending on the first path taken. (no one-way gadget used)
 
 """
 either_trisum_nodes = [
-    Node('initial', Initial(free=('T',), fixed=(('n', '4'),))),
-    Node('decr_x', Subtraction('n', '1')),
-    Node('sub_t_x', Subtraction('T', 'n')),
-    Node('none', None),
-    Node('decr_t', Subtraction('T', '1')),
-    Node('terminal', Terminal()),
+    Node("initial", Initial(free=["T"], fixed=[("n", 4)])),
+    Node("decr_x", Subtraction("n", "1")),
+    Node("sub_t_x", Subtraction("T", "n")),
+    Node("none", None),
+    Node("decr_t", Subtraction("T", "1")),
+    Node("terminal", Terminal()),
 ]
 either_trisum_d = {n.name: n for n in either_trisum_nodes}
 
 either_trisum_graph = Graph(
     nodes=list(either_trisum_d.values()),
     edges=[
-        (either_trisum_d['initial'], either_trisum_d['decr_x']),
-        (either_trisum_d['decr_x'], either_trisum_d['sub_t_x']),
-        (either_trisum_d['sub_t_x'], either_trisum_d['none']),
-        (either_trisum_d['none'], either_trisum_d['initial']),
-        (either_trisum_d['none'], either_trisum_d['decr_t']),
-        (either_trisum_d['decr_t'], either_trisum_d['terminal']),
+        (either_trisum_d["initial"], either_trisum_d["decr_x"]),
+        (either_trisum_d["decr_x"], either_trisum_d["sub_t_x"]),
+        (either_trisum_d["sub_t_x"], either_trisum_d["none"]),
+        (either_trisum_d["none"], either_trisum_d["initial"]),
+        (either_trisum_d["none"], either_trisum_d["decr_t"]),
+        (either_trisum_d["decr_t"], either_trisum_d["terminal"]),
     ],
 )
-
-print(either_trisum_graph)
-
