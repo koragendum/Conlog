b738af01c0bae24aa521e70580fa3159c905bbf4
kade <kade@protonmail.ch>
2022-04-30 18:54:23 -0400

    change to frontend errors

--- conlog/frontends.py
+++ conlog/frontends.py
@@ -8,7 +8,7 @@ import re
 #   statement = var-name "=" (const | "?")
 #             | node ("--" node)+
 #
-#   program := statement (";" statement)* ";"?
+#   program = statement (";" statement)* ";"?
 #
 #   Variables that are not explicitly set to a constant or "?" are implictly
 #   left free. Nodes that are never declared are assumed to be None nodes.
@@ -39,6 +39,54 @@ class Token:
             return f"{tk} : {self.kind} @ {self.line},{self.column}"
         value = repr(self.value) if self.kind == 'string' else self.value
         return f"{tk} {value} : {self.kind} @ {self.line},{self.column}"
+
+
+def extract_tokens(obj):
+    if isinstance(obj, Token):
+        return [obj]
+    if isinstance(obj, (list, tuple)):
+        return sum((extract_tokens(x) for x in obj), [])
+    raise RuntimeError(f"unable to extract tokens from {type(obj)}")
+
+
+class FrontendError:
+    def __init__(self, msg, hi):
+        """
+        msg -- string describing the failure
+        hi  -- token or parse tree
+        """
+        self.message = msg
+        self.highlight = hi
+
+    def __repr__(self):
+        return f"\x1B[91merror\x1B[39m: {self.message}"
+
+    def show(self, log_lines):
+        """
+        log_lines -- a copy of the text (prior to tokenization) split into lines
+        """
+        if self.highlight is None:
+            print(f"\x1B[91merror\x1B[39m: {self.message}")
+            return
+        tokens = extract_tokens(self.highlight)
+        top = tokens[ 0].line - 1
+        bot = tokens[-1].line - 1
+        print(f"\x1B[91merror\x1B[39m: line {tokens[0].line}: " + self.message)
+        if bot-top > 1:
+            return  # not sure yet how to display multi-line errors
+        line = log_lines[top]
+        margin = "\x1B[2m\u2502\x1B[22m "
+        print(margin)
+        print(margin + line)
+        print(margin, end='')
+        left  = tokens[ 0].column - 1
+        right = tokens[-1].column - 1 + len(tokens[-1].text)
+
+        print(" "*left, end='')
+        print("\x1B[91m^", end='')
+        print("~"*(right-left-1), end='')
+        print("\x1B[39m", end='')
+        print()
 
 
 class TokenStream:
@@ -132,57 +180,8 @@ class TokenStream:
                 self.text = self.text[match.end():]
                 return Token(char_text, char_text[1:], 'character', tok_line, tok_column)
 
-            raise RuntimeError(f"unable to tokenize ({tok_line}, {tok_column})")
-
-
-def extract_tokens(obj):
-    if isinstance(obj, Token):
-        return [obj]
-    if isinstance(obj, ParseTree):
-        return sum((extract_tokens(x) for x in obj.children), [])
-    if isinstance(obj, (list, tuple)):
-        return sum((extract_tokens(x) for x in obj), [])
-    raise RuntimeError(f"unable to extract tokens from {type(obj)}")
-
-
-class ParseFailure:
-    def __init__(self, msg, hi):
-        """
-        msg -- string describing the failure
-        hi  -- token or parse tree
-        """
-        self.message = msg
-        self.highlight = hi
-
-    def __repr__(self):
-        return f"\x1B[91merror\x1B[39m: {self.message}"
-
-    def show(self, log_lines):
-        """
-        log_lines -- a copy of the text (prior to tokenization) split into lines
-        """
-        if self.highlight is None:
-            print(f"\x1B[91merror\x1B[39m: {self.message}")
-            return
-        tokens = extract_tokens(self.highlight)
-        top = tokens[ 0].line - 1
-        bot = tokens[-1].line - 1
-        print(f"\x1B[91merror\x1B[39m: line {tokens[0].line}: " + self.message)
-        if bot-top > 1:
-            return  # not sure yet how to display multi-line errors
-        line = log_lines[top]
-        margin = "\x1B[2m\u2502\x1B[22m "
-        print(margin)
-        print(margin + line)
-        print(margin, end='')
-        left  = tokens[ 0].column - 1
-        right = tokens[-1].column - 1 + len(tokens[-1].text)
-
-        print(" "*left, end='')
-        print("\x1B[91m^", end='')
-        print("~"*(right-left-1), end='')
-        print("\x1B[39m", end='')
-        print()
+            err_tok = Token(self.text[0], None, None, tok_line, tok_column)
+            return FrontendError("unable to tokenize", [err_tok])
 
 
 class TextProgram:
@@ -195,15 +194,15 @@ class TextProgram:
 
     def add_node(self, seq):
         """
-        Returns (number of tokens consumed, node name) or instance of ParseFailure.
+        Returns (number of tokens consumed, node name) or instance of FrontendError.
         seq -- nonempty list of Tokens
         """
         if seq[0].kind != 'name':
-            return ParseFailure("expected node name", seq[0])
+            return FrontendError("expected node name", seq[0])
 
         node_name = seq[0].value
         if node_name in self.variables:
-            return ParseFailure("variable with this name already exists", seq[0])
+            return FrontendError("variable with this name already exists", seq[0])
 
         if len(seq) == 1 or not (seq[1].kind == 'symbol' and seq[1].value == '['):
             if node_name in self.unknown:
@@ -213,28 +212,28 @@ class TextProgram:
             return (1, node_name)
 
         if len(seq) < 6:
-            return ParseFailure("incomplete node definition", seq)
+            return FrontendError("incomplete node definition", seq)
 
         if seq[2].kind != 'name':
-            return ParseFailure("expected variable name", seq[2])
+            return FrontendError("expected variable name", seq[2])
 
         if not (seq[3].kind == 'symbol' and seq[3].value in ('+=', '-=')):
-            return ParseFailure("expected increment or decrement", seq[3])
+            return FrontendError("expected increment or decrement", seq[3])
 
         if seq[4].kind not in ('numeric', 'character', 'name'):
-            return ParseFailure("expected literal or variable name", seq[5])
+            return FrontendError("expected literal or variable name", seq[5])
 
         if not (seq[5].kind == 'symbol' and seq[5].value == ']'):
-            return ParseFailure("expected a closing bracket", seq[5])
+            return FrontendError("expected a closing bracket", seq[5])
 
         if seq[2].value in self.nodes:
-            return ParseFailure("label refers to an existing node", seq[2])
+            return FrontendError("label refers to an existing node", seq[2])
 
         if seq[4].kind == 'name' and seq[4].value in self.nodes:
-            return ParseFailure("label refers to an existing node", seq[4])
+            return FrontendError("label refers to an existing node", seq[4])
 
         if node_name in self.nodes and self.nodes[node_name] is not None:
-            return ParseFailure("node mutation has already been defined", seq[1:6])
+            return FrontendError("node mutation has already been defined", seq[1:6])
 
         lhs = seq[2].value
         if lhs in self.unknown:
@@ -268,11 +267,11 @@ class TextProgram:
 
     def add_statement(self, seq):
         """
-        Returns None or instance of ParseFailure.
+        Returns None or instance of FrontendError.
         seq -- nonempty list of Tokens
         """
         if seq[0].kind != 'name':
-            return ParseFailure("statement must begin with a name", seq[0])
+            return FrontendError("statement must begin with a name", seq[0])
 
         # Simple declaration
         name = seq[0].value
@@ -284,18 +283,18 @@ class TextProgram:
         # Variable initialization
         if seq[1].kind == 'symbol' and seq[1].value == '=':
             if len(seq) < 3:
-                return ParseFailure("incomplete variable initialization", seq)
+                return FrontendError("incomplete variable initialization", seq)
             is_literal = seq[2].kind in ('numeric', 'character')
             is_free    = seq[2].kind == 'symbol' and seq[2].value == '?'
             if not (is_literal or is_free):
-                return ParseFailure("variable must be initialized to a constant or marked free", seq[2])
+                return FrontendError("variable must be initialized to a constant or marked free", seq[2])
             if len(seq) > 3:
-                return ParseFailure("extraneous characters in variable intialization", seq[3:])
+                return FrontendError("extraneous characters in variable intialization", seq[3:])
             if name in self.nodes:
-                return ParseFailure("node with this label already exists", seq[0])
+                return FrontendError("node with this label already exists", seq[0])
             if name in self.variables:
                 if self.variables[name] is not None:
-                    return ParseFailure("variable has already been initialized", seq[0])
+                    return FrontendError("variable has already been initialized", seq[0])
             if name in self.unknown:
                 self.unknown.remove(name)
             if is_free:
@@ -311,13 +310,13 @@ class TextProgram:
         last_node = None
         while True:
             result = self.add_node(seq[index:])
-            if isinstance(result, ParseFailure):
+            if isinstance(result, FrontendError):
                 return result
 
             consumed, this_node = result
             if last_node is not None:
                 if this_node == last_node:
-                    return ParseFailure("cannot make edge from a node to itself", seq[index])
+                    return FrontendError("cannot make edge from a node to itself", seq[index])
                 canonical = tuple(sorted((last_node, this_node)))
                 self.edges.add(canonical)
             last_node = this_node
@@ -326,7 +325,7 @@ class TextProgram:
             if index >= len(seq):
                 return
             if not (seq[index].kind == 'symbol' and seq[index].value == '--'):
-                return ParseFailure("expected edge", seq[index])
+                return FrontendError("expected edge", seq[index])
             index += 1
 
     def validate(self):
@@ -342,6 +341,8 @@ if __name__ == '__main__':
         buf = []
         while True:
             tok = next(stream)
+            if isinstance(tok, FrontendError):
+                return tok
             if tok is None:
                 return None if len(buf) == 0 else buf
             if tok.kind == 'symbol' and tok.value == ';':
@@ -360,8 +361,13 @@ if __name__ == '__main__':
 
     while True:
         seq = readline(stream)
+        if isinstance(seq, FrontendError):
+            log_lines = stream.log.split('\n')
+            seq.show(log_lines)
+            stream.text = ""
+            continue
         result = program.add_statement(seq)
-        if isinstance(result, ParseFailure):
+        if isinstance(result, FrontendError):
             log_lines = stream.log.split('\n')
             result.show(log_lines)
         print('Unk:   ', program.unknown)
