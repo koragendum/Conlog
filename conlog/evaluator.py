91ebe71d2d27d32dfa064495a19072b2c2cf5167
Patrick Steele <steele.pat@gmail.com>
2022-05-01 13:05:25 -0400

    Support partial path evaluation

--- conlog/evaluator.py
+++ conlog/evaluator.py
@@ -14,14 +14,7 @@ from conlog.datatypes import (
 )
 
 
-def evaluate(path: list[Node], assignment: dict[str, int]) -> Solution | None:
-    initial = cast(Initial, path[0].op)
-
-    # Verify assignments agree with boundary conditions
-    for x, y in initial.fixed:
-        if assignment[x] != y:
-            return None
-
+def partial_evaluate(path: list[Node], assignment: dict[str, int]) -> tuple[dict[str, int], list[str]]:
     var_values = dict(assignment)
     prints = []
 
@@ -68,16 +61,32 @@ def evaluate(path: list[Node], assignment: dict[str, int]) -> Solution | None:
                 if rvalue > 0:
                     var_values[lhs] -= 1
             case Terminal():
-                for _, x in var_values.items():
-                    if x != 0:
-                        return None
-                found_terminal = True
+                return var_values, prints
             case _:
                 raise ValueError(f"Unknown operation: {node.op}")
 
         if found_terminal:
             break
-    else:
-        raise ValueError("Path does not end in Terminal")
+
+    return var_values, prints
+
+
+def evaluate(path: list[Node], assignment: dict[str, int]) -> Solution | None:
+    initial = cast(Initial, path[0].op)
+
+    # Verify assignments agree with boundary conditions
+    for x, y in initial.fixed:
+        if assignment[x] != y:
+            return None
+
+    var_values, prints = partial_evaluate(path, assignment)
+
+    match path[-1].op:
+        case Terminal():
+            for _, x in var_values.items():
+                if x != 0:
+                    return None
+        case _:
+            raise ValueError(f"Unknown operation: {path[-1].op}")
 
     return Solution(path, assignment, prints)
