3ba3a18be0bda5b04b1a361b57f720e27872b06b
Geronimo Mirano <>
2022-05-01 14:48:05 -0400

    Only import matplotlib on plot

--- conlog/plot.py
+++ conlog/plot.py
@@ -1,8 +1,9 @@
-import matplotlib.pyplot as plt
 import networkx as nx
 
 
 def plot_graph(g: nx.Graph) -> None:
+    import matplotlib.pyplot as plt
+
     fig = plt.figure(figsize=(20, 20))
 
     ax = fig.add_subplot(1, 1, 1)
