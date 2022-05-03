73afc04ec500beba97e8604b2b467abd3cc6c48c
Geronimo Mirano <>
2022-05-03 09:21:26 -0400

    Added Cython build.py and C solver.

---
+++ conlog/solver_c_fast.h
@@ -0,0 +1,55 @@
+// Solve the conlog grid using BFS --  C
+
+#include <stdint.h>
+
+#define MAX_DEGREE 16
+#define MAX_NUM_VALUES 32
+#define MAX_QUEUE_LENGTH 10000000
+
+
+#define Initial 1
+#define Terminal 2
+#define Addition 3
+#define IntegerPrint 4
+#define UnicodePrint 5
+#define Subtraction 6
+#define ConditionalIncrement 7
+#define ConditionalDecrement 8
+#define NoneType 9
+
+
+typedef struct CNode {
+    uint8_t node_type;
+    uint64_t node_i;
+    int64_t lhs;  // Index of the lhs operand
+    uint8_t rhs_is_constant;  // (bool) Whether rhs is a constant or an index
+    int64_t rhs;  // Index/value of the rhs operand
+    uint8_t num_neighbors;  // Number of neighbors of this node
+    struct CNode * neighbor_arr[MAX_DEGREE];  // (Yes, a POINTER ARRAY)
+} CNode;
+
+
+typedef struct CSearchState {
+    CNode * node;
+    CNode * last_node;
+    int64_t values[MAX_NUM_VALUES];
+    struct CSearchState * parent_search_state;
+} CSearchState;
+
+
+typedef struct CSearchWorkspace {
+    CSearchState search_queue[MAX_QUEUE_LENGTH];
+    CSearchState * search_queue_next_free;  // Add things via: *search_queue_next_free = thing; search_queue_next_free++
+    CSearchState * search_queue_next_to_pop;  // Add things via: thing = *search_queue_next_to_pop; search_queue_next_to_pop++
+    CNode * node_arr;  // CNode[num_nodes]
+    uint64_t num_values;  // Number of values of the graph
+    uint64_t num_free_values;  // Number of free values of the graph
+    uint64_t num_fixed_values;  // Number of fixed values of the graph
+    int64_t * fixed_values;  // int64_t[num_fixed_values]  The values themselves
+    CNode * terminal_node;  // CNode *
+    uint64_t iterations;
+    uint64_t limit;
+    int64_t * lower_bounds;
+    int64_t * upper_bounds;
+} CSearchWorkspace;
+
